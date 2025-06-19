c1d07d99e5e35a3a4b9fc54ba599b5595fbe2460
Omar Tegar <80182433+medo1200@users.noreply.github.com>
2025-06-19 18:32:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const
     express = require('express'),
     app = express(),
     pg = require('pg'),
-    DATABASE = process.env.DATABASE_PUBLIC_URL,
+    DATABASE = process.env.DATABASE,
     PORT = process.env.PORT || 3000,
     client = new pg.Client(DATABASE);
 
